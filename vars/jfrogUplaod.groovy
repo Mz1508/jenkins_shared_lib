27b2284283cee47694b63c35d11d1187f4fe78e2
Mz1508 <136124542+Mz1508@users.noreply.github.com>
2023-11-27 17:55:49 -0500

    Update jfrogUplaod.groovy

--- vars/jfrogUplaod.groovy
+++ vars/jfrogUplaod.groovy
@@ -1,3 +1,3 @@
 def call() {
-sh curl -X PUT -u admin:Admin123 -T ./var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.142.47.84:8082/artifactory/example-repo-local
+sh """ curl -X PUT -u admin:Admin123 -T ./var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.142.47.84:8082/artifactory/example-repo-local/ """
   }
