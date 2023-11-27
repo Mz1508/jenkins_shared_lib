d66d1bf4ce1de7d027af8b31b7efa7828a43d217
Mz1508 <136124542+Mz1508@users.noreply.github.com>
2023-11-27 17:27:21 -0500

    Update jfrogUplaod.groovy

--- vars/jfrogUplaod.groovy
+++ vars/jfrogUplaod.groovy
@@ -1,22 +1,3 @@
-echo 'jfrog start...........................'
-                    def server = Artifactory.newServer url: registry + "/artifactory", credentialsId: "Jfrog"
-                    echo '1'
-                    def properties = "buildid=${env.BUILD_ID},commitid=${GIT_COMMIT}";
-                    echo '2'
-                    def uploadSpec = """{
-                        "files": [
-                            {
-                                "pattern": "target/(*)",
-                                "target": "libs-release-local/{1}",
-                                "flat": "false",
-                                "props": "${properties}",
-                                "exclusions": ["*.sha1", "*.md5"]
-                            }
-                        ]
-                    }"""
-                    echo '3'
-                    def buildInfo = server.upload(uploadSpec)
-                    echo '4'
-                    buildInfo.env.collect()
-                    server.publishBuildInfo(buildInfo)
-                    echo '<--------------- Jar Publish Ended --------------->'
+def call() {
+sh curl-X PUT -u admin:Admin123 -T ./var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.142.47.84:8082/artifactory/example-repo-local
+  }
